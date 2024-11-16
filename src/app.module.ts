2bf54891916dc5a320ace10e4ab2278b1bb9c0fd
EpsithO <matheodu25@gmail.com>
2024-11-16 15:06:45 +0100

    base de données préparé + ORM configuré

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'db_gouv',
+      entities: [__dirname + '/Entity/*.entity.ts'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
